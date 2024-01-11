abb1281cfa9c2c94ac9131a844908a9488afb41e
vceniceros <85922025+vceniceros@users.noreply.github.com>
2024-01-11 13:28:15 -0300

    implementacion del error 404

--- nmt_page/src/Router.tsx
+++ nmt_page/src/Router.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import home from "./components/Home";
 import about_us from "./components/About_us";
 import Contact from "./components/Contact";
-
+import Error404 from "./components/Error404";
 
 class Router extends Component{
     render(): React.ReactNode {
@@ -13,6 +13,7 @@ class Router extends Component{
                     <Route path="/" Component={home}></Route>
                     <Route path="/about_us" Component={about_us}></Route>
                     <Route path="/contact" Component={Contact}></Route>
+                    <Route path="*" Component={Error404}></Route>
                 </Routes>
             </BrowserRouter>
         );
